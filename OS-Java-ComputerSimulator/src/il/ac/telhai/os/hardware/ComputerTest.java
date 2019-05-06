0735590f68dd0baa45f747b6f46123ead03d4e51
Mordo Shalom <cmshalom@cs.technion.ac.il>
2019-05-06 13:52:57 +0300

    Test fixed

--- OS-Java-ComputerSimulator/src/il/ac/telhai/os/hardware/ComputerTest.java
+++ OS-Java-ComputerSimulator/src/il/ac/telhai/os/hardware/ComputerTest.java
@@ -17,9 +17,9 @@ public class ComputerTest extends Computer {
 		assertEquals(		
 				"\nCS=0	DS=1	SS=0	ES=2" +
 				"\nAX=4	BX=0	CX=0	DX=0" +
-				"\nSP=96	IP=22	SI=0	DI=0" +
+				"\nSP=96	IP=23	SI=0	DI=0" +
 				"\nBP=0	FL=2\t", c.cpu.getRegisters());
-		assertNull(ProcessControlBlock.getProcess(1));
+		assertEquals(1, ProcessControlBlock.getProcess(1).getId());
 		assertNull(ProcessControlBlock.getProcess(2));
 		assertNull(ProcessControlBlock.getProcess(3));
 		assertEquals(1, ProcessControlBlock.getProcess(4).getParent().getId());
