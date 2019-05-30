3b9f294bccd909744c2fab9431439b7175b3146d
cmshalom <cmshalom@cs.technion.ac.il>
2019-05-30 08:10:22 +0300

    Lab 10 solution

--- OS-Java-ComputerSimulator/src/il/ac/telhai/os/software/VMM.java
+++ OS-Java-ComputerSimulator/src/il/ac/telhai/os/software/VMM.java
@@ -34,15 +34,53 @@ public class VMM implements InterruptHandler {
 	}
 
 	private int getFreePage() {
-		//        		logger.trace("Allocating segment " + i);
+		mmu.enterRealMode();
+        for (int i=1; i<numberOfRealSegments; i++) {
+            if (mmu.readByte(0, i) == 0) {
+            	mmu.writeByte(0, i, (byte) 1); 
+        		mmu.exitRealMode();
+        		logger.trace("Allocating segment " + i);
+        		return i;            	
+            }
+        }
+        throw new IllegalStateException("Real Memory Full");
 	}
-
+	
 	public PageTableEntry[] clonePageTable(PageTableEntry[] pageTable) {
-		//        		logger.trace("Sharing segment " + ret[i].getSegmentNo() + ", refcnt=" + (refCount+1));
+		mmu.enterRealMode();
+		PageTableEntry[] ret = new PageTableEntry[pageTable.length];
+		for (int i = 0; i < pageTable.length; i++) {
+			ret[i] = new PageTableEntry(pageTable[i]);
+			if (ret[i].isMappedtoMemory() || ret[i].isMappedtoDisc()) {
+				ret[i].setCopyOnWrite(true);			
+				pageTable[i].setCopyOnWrite(true);
+				byte refCount = mmu.readByte(0, ret[i].getSegmentNo());
+				if (refCount==127) {
+					throw new IllegalStateException("Reference count reached 127 in page sharing");
+				}
+        		logger.trace("Sharing segment " + ret[i].getSegmentNo() + ", refcnt=" + (refCount+1));
+				mmu.writeByte(0, ret[i].getSegmentNo(), (byte) (refCount+1));
+			}
+		}
+		mmu.exitRealMode();
+		return ret;
 	}
-
+	
 	public void releasePageTable (PageTableEntry[] pageTable) {
-		//				logger.trace("Releasing segment " + e.getSegmentNo() + " ,refcnt=" + (refCount-1));
+		// TODO: What happens if parent exits before children?
+		//       or parent wants to write 
+		//       Shared pages should not be released. 
+		mmu.enterRealMode();
+		for (int i=0; i<pageTable.length; i++) {
+            PageTableEntry e = pageTable[i];
+            pageTable[i] = null;
+			if (e.isMappedtoMemory()) {
+				byte refCount = mmu.readByte(0, e.getSegmentNo());
+				mmu.writeByte(0, e.getSegmentNo(), (byte) (refCount-1));
+				logger.trace("Releasing segment " + e.getSegmentNo() + " ,refcnt=" + (refCount-1));
+			}
+		}
+		mmu.exitRealMode();
 	}
 
 
@@ -52,23 +90,29 @@ public class VMM implements InterruptHandler {
 		PageTableEntry entry = fault.getEntry();
 		assert(entry != null);
 		if (entry.isMappedtoMemory()) {
-			int newSegment = getFreePage();  // Recall that getFreePage exits realMode
-			logger.trace("Copying segment " + entry.getSegmentNo() + " to segment " + newSegment);
-			mmu.copySegment(newSegment, entry.getSegmentNo());
-			entry.setSegmentNo(newSegment);				
+			mmu.enterRealMode();
+			byte refCount = mmu.readByte(0, entry.getSegmentNo());
+			if (refCount > 1) {  // There are at least one more process sharing the page
+				mmu.writeByte(0, entry.getSegmentNo(), (byte) (refCount-1));
+				int newSegment = getFreePage();  // Recall that getFreePage exits realMode
+				logger.trace("Copying segment " + entry.getSegmentNo() + " to segment " + newSegment);
+				mmu.copySegment(newSegment, entry.getSegmentNo());
+				entry.setSegmentNo(newSegment);				
+			}
+			mmu.exitRealMode();
 			entry.setCopyOnWrite(false);
 		} else {
 			entry.setSegmentNo(getFreePage());
 			entry.setMappedToMemory(true);			
 		}
 	}
-
+	
 	void shutdown( ) {
 		mmu.enterRealMode();
 		int numberOfFreePages = 0;
-		for (int i=1; i<numberOfRealSegments; i++) {
-			if (mmu.readByte(0, i) == 0) numberOfFreePages++;
-		}
+        for (int i=1; i<numberOfRealSegments; i++) {
+            if (mmu.readByte(0, i) == 0) numberOfFreePages++;
+        }
 		mmu.exitRealMode();
 		logger.info("Free Memory: " + numberOfFreePages + " pages.");
 	}
